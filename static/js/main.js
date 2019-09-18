baa8d11e002568ee5bcc40e9f73668dd94c734ee
jugonzalez40 <juanmarfil9696@gmail.com>
2019-09-17 20:11:38 -0500

    responsive

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,15 @@ $(function() {
     //Active glide
     new Glide('.glide', { autoplay: 5000 }).mount()
     new Glide('.glide-testimony', { autoplay: 5000 }).mount()
-        //Active Scroll Animations
+    //Active Scroll Animations
     AOS.init();
+
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector(`#${burger.dataset.target}`);
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('is-active');
+        nav.classList.toggle('is-active');
+    });
+
 });
